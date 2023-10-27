36c7001f92bc0e0ecf4535ae341dcd1e07a6633f
Anastasia Fongrad <fongradnastya@gmail.com>
2023-10-27 17:57:02 +0700

    Started a database

---
+++ src/main/java/com/example/demo/Clothes.java
@@ -0,0 +1,80 @@
+package com.example.demo;
+
+public class Clothes {
+    private int id;
+    private String name;
+    private String color;
+    private String brand;
+    private int size;
+    private double price;
+    private int quantity;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public String toString() {
+        return "Clothes{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", brand='" + brand + '\'' +
+                ", size=" + size +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                '}';
+    }
+}
